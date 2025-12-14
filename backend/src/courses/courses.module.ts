d98d0e400dd170eff6e8f3171468a785374869ae
sorawalker <nikitka.grishanov@gmail.com>
2025-12-14 23:59:39 +0300

    fix: REesolve course module dependences

--- backend/src/courses/courses.module.ts
+++ backend/src/courses/courses.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 import { LoggerModule } from '../common/logger/logger.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   controllers: [CoursesController],
   providers: [CoursesService],
-  imports: [LoggerModule],
+  imports: [LoggerModule, UsersModule],
   exports: [CoursesService],
 })
 export class CoursesModule {}
